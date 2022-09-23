f8127d29f76a155dd1f0252bd4db7f6eb0a028b3
Bernardngari <bernardngari.ke@gmail.com>
2022-09-23 21:44:22 +0300

    commit tree 2cd61903d090ed61e12af00bf69fba8ece7d7983

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,9 @@ class StudentsController < ApplicationController
 		students_by_grades_ascending = Student.all.order(grade: :desc)
 		render json: students_by_grades_ascending
 	end
+
+	def highest_grade
+		student_highest_grade = Student.order(grade: :desc).first
+		render json: student_highest_grade
+	end
 end
